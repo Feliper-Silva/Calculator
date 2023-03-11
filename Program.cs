01bb4f8d74a2d5ee9beb0c684efbf540cf61e7fc
Felipe Silvam <felipe726silva@gmail.com>
2023-03-11 17:03:32 -0300

    Mudança na estrutura da calculadora utilizando funções.

--- Program.cs
+++ Program.cs
@@ -6,72 +6,83 @@ namespace Calculator
     {
         static void Main (string[] args) 
         {
-            Console.Clear();
-            Console.Write("Bem Vindo(a) a nossa primeira calculadora em c#\nDeseja Iniciar ?\n1 - Iniciar\n2 - Encerrar Calculadora\n");
-            int Opcao = int.Parse(Console.ReadLine());
+            int Opcao = Menu();
             while(Opcao == 1)
             {           
-                int count =0;
-                Console.WriteLine("Digite o primeiro valor");
-                float Number1 = float.Parse(Console.ReadLine());
-                Console.WriteLine("Digite o segundo número: ");
-                float Number2 = float.Parse(Console.ReadLine());
-
                 Console.Write("Escolha uma opção abaixo:\n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Encerrar Calculadora\n");
                 int Acao = int.Parse(Console.ReadLine());
 
-
-                float Result=0;
-
                 switch (Acao){
                     case 1:
-                        Result = Soma (Number1,Number2);
-                        Console.WriteLine($"O resultado da Soma :\n{Number1}  + {Number2} = {Result}");
-                        count ++;
+                         Soma ();
                         break;
                     case 2:
-                        Result = Subtracao (Number1,Number2);
-                        Console.WriteLine($"O resultado da Soma :\n{Number1}  + {Number2} = {Result}");
-                        count ++;
+                        Subtracao ();
                         break;
                     case 3:
-                        Result = Multiplicacao (Number1,Number2);
-                        Console.WriteLine($"O resultado da Multiplicação :\n{Number1}  * {Number2} = {Result}");
-                        count ++;
+                        Multiplicacao ();
                         break;
                     case 4:
-                        Result = Divisao (Number1,Number2);
-                        Console.WriteLine($"O resultado da Multiplicação :\n{Number1}  / {Number2} = {Result}");
-                        count ++;
+                        Divisao ();
                         break;
                     case 5:
                         Opcao ++;
                         break;
                     default:
-                        Console.WriteLine("opção invalida");
+                        Menu();
                         break;
                 }
             }
         }
 
-        static float Soma (float number1, float number2)
+        static int Menu ()
         {
-            return number1 + number2;                
+            Console.Clear();
+            Console.Write("Bem Vindo(a) a nossa primeira calculadora em c#\nDeseja Iniciar ?\n1 - Iniciar\n2 - Encerrar Calculadora\n----------------\n");
+            return int.Parse(Console.ReadLine());
         }
 
-        static float Subtracao (float number1, float number2)
+        static void Soma ()
         {
-            return number1 - number2;                
+            Console.WriteLine("Digite o primeiro valor");
+            float Number1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número: ");
+            float Number2 = float.Parse(Console.ReadLine());
+            float Result = Number1 + Number2;
+            Console.WriteLine($"O resultado da Soma é:\n{Number1}  + {Number2} = {Result}");
+             
         }
 
-        static float Multiplicacao (float number1, float number2)
+        static void Subtracao ()
         {
-            return number1 * number2;
+            Console.WriteLine("Digite o primeiro valor");
+            float Number1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número: ");
+            float Number2 = float.Parse(Console.ReadLine());
+            float Result = Number1 - Number2;
+            Console.WriteLine($"O resultado da Subtração é:\n{Number1}  + {Number2} = {Result}\n");
+           
         }
 
-        static float Divisao (float number1, float number2)
+        static void Multiplicacao ()
         {
-            return number1 / number2;
+            Console.WriteLine("Digite o primeiro valor");
+            float Number1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número: ");
+            float Number2 = float.Parse(Console.ReadLine());
+            float Result = Number1 * Number2;
+            Console.WriteLine($"O resultado da Multiplicação :\n{Number1}  * {Number2} = {Result}\n");
+        }
+
+        static void Divisao ()
+        {
+            Console.WriteLine("Digite o primeiro valor");
+            float Number1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número: ");
+            float Number2 = float.Parse(Console.ReadLine());
+            float Result = Number1 / Number2;
+            Console.WriteLine($"O resultado da Divisão :\n{Number1}  / {Number2} = {Result}\n");
+
         }
     }
 }
